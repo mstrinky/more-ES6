299c95e2acb8729f7574d4b9530f9acd691e87e3
Mst Rinky Akter <mstrinkya@gmail.com>
2023-05-17 23:30:39 +0600

    arrow

---
+++ problem.js
@@ -0,0 +1,15 @@
+// one
+const numbers= (a,b,c) => a*b*c;
+const multiply  = numbers(2,3,4);
+console.log(multiply );
+
+// two 
+const lines = `I am a coder. 
+I love to code. 
+I love to eat biriyani`;
+console.log(lines);
+
+// three 
+const number = (a,b=3)=>a+b;
+const add = number(2);
+console.log(add);
